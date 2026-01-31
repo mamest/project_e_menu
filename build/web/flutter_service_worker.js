f3b322d9b82a45a027f8bcf7cd74617cee0bf2eb
Marvin Mester <marvin.mester@googlemail.com>
2026-01-31 21:22:02 +0100

    Add pre-built web files for Vercel

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1ef2dd970c26edee97a8bfeb0d9b7b4d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3deb5a43a15c47775ec13be2c16de607",
+"flutter_bootstrap.js": "8231b6338f0a91cd4ec77791f93b0624",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
